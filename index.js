a1454df5ccfb2c1e015c9e8775176728ae10d96c
Jahdon Ervin <Jahdonervin@gmail.com>
2022-12-14 17:27:41 -0500

    commit tree 104748ae6d90a6170f6b5ad71acc1645c307c766

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const num3 = 20;
+const num4 = 8;
+const mod =  num3 % num4;
+
+const arr = [5, 10, 15 ,20];
+const max = Math.max(...arr);
+
+const random = Math.floor(Math.random() * 10) + 1;
